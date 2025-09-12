bf92af479ce754a02e0d6663ce54f52241203299
olga_grin <olgagrinberg@users.noreply.github.com>
2025-09-11 22:45:18 -0400

    Update java-datatypes.java

--- java-datatypes.java
+++ java-datatypes.java
@@ -20,6 +20,9 @@ class Solution{
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
                 //Complete the code
+                if(x>=-32768 && x<=32767)System.out.println("* short");
+                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
+                if(x>=-9223372036854775808L && x<=9223372036854775807L)System.out.println("* long");
             }
             catch(Exception e)
             {
