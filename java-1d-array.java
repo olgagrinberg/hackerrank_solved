82a721593283a7f7fade4ecc142eeedc6eec74a8
olga_grin <olgagrinberg@users.noreply.github.com>
2025-09-12 12:36:13 -0400

    Create java-1d-array.java

---
+++ java-1d-array.java
@@ -0,0 +1,26 @@
+//https://www.hackerrank.com/challenges/java-1d-array-introduction/problem?isFullScreen=true
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        // Create an array to hold n integers
+        int[] a = new int[n];
+
+        // Read n integers and store them in the array
+        for (int i = 0; i < n; i++) {
+            a[i] = scan.nextInt();
+        }
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
