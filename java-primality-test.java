83633f9136c58329d04613c479e55d377e8b1e7f
olga_grin <olgagrinberg@users.noreply.github.com>
2025-09-12 12:27:30 -0400

    Create java-primality-test.java

---
+++ java-primality-test.java
@@ -0,0 +1,29 @@
+//https://www.hackerrank.com/challenges/java-primality-test/problem?isFullScreen=true
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String n = bufferedReader.readLine();
+        
+        BigInteger big = new BigInteger(n);
+        
+        if (big.isProbablePrime(10)) {
+            System.out.println("prime");
+        } else {
+            System.out.println("not prime");
+        }
+
+        bufferedReader.close();
+    }
+}
