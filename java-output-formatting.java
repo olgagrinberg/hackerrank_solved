65ae2539c4dc94f619720c9ac4f1b534fe96fe00
olga_grin <olgagrinberg@users.noreply.github.com>
2025-09-11 21:30:36 -0400

    Create java-output-formatting.java

---
+++ java-output-formatting.java
@@ -0,0 +1,18 @@
+//https://www.hackerrank.com/challenges/java-output-formatting/problem?isFullScreen=true
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("================================");
+            for(int i=0;i<3;i++){
+                String s1=sc.next();
+                int x=sc.nextInt();
+                System.out.printf("%-15s%0,3d\n",s1,x);
+            }
+            System.out.println("================================");
+
+    }
+}
+
